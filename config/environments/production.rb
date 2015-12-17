1dc393b309cd6f8ec8ed768a92395144982d569d
Darren Burgess <darrentburgess@gmail.com>
2015-12-17 14:24:36 +0000

    fix type

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   host = 'blooming-temple-6812.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address              => 'smtp.sendgrid.com'
+    :address              => 'smtp.sendgrid.com',
     :port                 => '587',
     :authentication       => :plain,
     :user_name            => ENV['SENDGRID_USERNAME'],
