ee33e3a5e4fca6250211ecbdf1d8f9573c7487cf
Darren Burgess <darrentburgess@gmail.com>
2015-12-21 01:23:15 +0000

    Add user microposts.  omitted prodution picture upload - requires AWS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
-    # redirect_to root_url and return unless !@user.activated?
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
   
   def new
@@ -54,15 +54,6 @@ class UsersController < ApplicationController
     end
     
     # before filters
-    
-    # confirms logged_in_user
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
     
     # confirms the correct user
     def correct_user
